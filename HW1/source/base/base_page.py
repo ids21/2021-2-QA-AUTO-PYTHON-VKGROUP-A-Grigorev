b7ea6b85faf8aac8edaad13080f7956057eb5d0d
ids21 <grinch.aleks@gmail.com>
2021-10-17 22:07:37 +0300

    Transfer wait logic to class Wait

--- HW1/source/base/base_page.py
+++ HW1/source/base/base_page.py
@@ -1,11 +1,38 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import (
     ElementClickInterceptedException,
-    StaleElementReferenceException,
 )
 from selenium.webdriver.support import expected_conditions as ExpCond
 from selenium.webdriver.common.by import By
 
+class Wait:
+
+    @staticmethod
+    def wait_until_precence_of_lement(driver, locator, timeout):
+        """waiting for the xpath element to appear on the page
+        """
+        return WebDriverWait(driver, timeout).until(
+            ExpCond.presence_of_element_located((By.XPATH, locator))
+            )
+        
+    @staticmethod
+    def wait_invisibility_of_element(driver, locator, timeout):
+        """
+        """        
+        return WebDriverWait(driver, timeout).until(
+                ExpCond.invisibility_of_element_located(
+                    (By.XPATH, locator)
+                )
+            )
+    
+    @staticmethod
+    def wait_element_to_be_clickable(driver, locator, timeout):
+        """
+        """        
+        return WebDriverWait(driver, timeout).until(
+            ExpCond.element_to_be_clickable((By.XPATH, locator))
+            )
+    
 
 class BasePage:
     
@@ -18,43 +45,55 @@ class BasePage:
 
     def click(self, locator: str, loading = False) -> None:
         if loading:
-            WebDriverWait(self.web_driver, 15).until(
-                ExpCond.invisibility_of_element_located(
-                    (By.XPATH, "//div[contains(@class,'spinner')]")
-                )
+            Wait().wait_invisibility_of_element(
+                driver = self.web_driver,
+                locator= "//div[contains(@class,'spinner')]", 
+                timeout = 15
             )
         try:
-            link = WebDriverWait(self.web_driver, 15).until(
-                ExpCond.presence_of_element_located((By.XPATH, locator))
+            link = Wait().wait_until_precence_of_lement(
+                driver = self.web_driver, 
+                locator = locator, 
+                timeout = 15
             )
             link.click()
         except ElementClickInterceptedException:
-            link = WebDriverWait(self.web_driver, 5).until(
-                ExpCond.element_to_be_clickable((By.XPATH, locator))
+            link = Wait().wait_element_to_be_clickable(
+                driver = self.web_driver, 
+                locator = locator, 
+                timeout = 5
             )
             link.click()
     
     def clear(self, locator: str) -> None:
-        link = WebDriverWait(self.web_driver, 15).until(
-                ExpCond.presence_of_element_located((By.XPATH, locator))
+        link = Wait().wait_until_precence_of_lement(
+                driver = self.web_driver, 
+                locator = locator, 
+                timeout = 15
             )
         link.clear()
     
     def keys(self, locator: str, text: str) -> None:
-        link = WebDriverWait(self.web_driver, 15).until(
-            ExpCond.presence_of_element_located((By.XPATH, locator))
-        )
+        link = Wait().wait_until_precence_of_lement(
+                driver = self.web_driver, 
+                locator = locator, 
+                timeout = 15
+            )
         link.send_keys(text)
     
     def is_enabled(self, locator: str) -> bool:
-        link = WebDriverWait(self.web_driver, 15).until(
-            ExpCond.presence_of_element_located((By.XPATH, locator))
-        )
+        link = Wait().wait_until_precence_of_lement(
+                driver = self.web_driver, 
+                locator = locator, 
+                timeout = 15
+            )
         return link.is_enabled()
 
     def get_attr(self, locator:str, attribute:str)-> str:
-        link = WebDriverWait(self.web_driver, 15).until(
-            ExpCond.presence_of_element_located((By.XPATH, locator))
-        )
+        link = Wait().wait_until_precence_of_lement(
+                driver = self.web_driver, 
+                locator = locator, 
+                timeout = 15
+            )
         text_atr: str = link.get_attribute(attribute)
         return text_atr
