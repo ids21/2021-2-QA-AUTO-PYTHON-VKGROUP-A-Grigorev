8d7cb80fcf277199b10365187fac71333c6c8d8a
ids21 <grinch.aleks@gmail.com>
2021-11-07 22:18:01 +0300

    added test for check delete segment

--- HW3/test_api/api_segments_test.py
+++ HW3/test_api/api_segments_test.py
@@ -7,23 +7,30 @@ from api_source.base_api import ApiBase
 class SegmentsBase(ApiBase):
 
     def check_create_segment(self, data):
-        get_segment = self.api_client.get_segment(data['id_after_add'])
+        get_segment = self.api_client.get_segment(data['id'])
         try:
-            assert get_segment['id'] == data['id_after_add']
+            assert get_segment['id'] == data['id']
             assert get_segment['name'] == data['name']
         except:
             assert False, "Failed to verify segment creation"
         finally:
-            self.api_client.post_delete_segment(data['id_after_add'])
+            self.api_client.post_delete_segment(data['id'])
             
     @pytest.fixture(scope="function")
     def segment(self) -> dict:
         segment = self.builder.create_segment_data()
 
         segment_json = self.api_client.post_create_segment(segment)
-        segment['id_after_add'] = segment_json['id']
 
-        return segment
+        return segment_json
+
+    def check_delete_segment(self, data):
+        try:
+            self.api_client.post_delete_segment(data['id'])
+        except:
+            assert False, "error occurred during delete segment"
+
+        
 
 @allure.epic("API")
 @allure.feature("Audiences")
@@ -33,3 +40,9 @@ class TestSegments(SegmentsBase):
     @pytest.mark.API
     def test_API_check_create_segment(self, segment):
         self.check_create_segment(segment)
+
+    @allure.story("Check delete segment")
+    @pytest.mark.API
+    def test_API_check_delete_segment(self, segment):
+        segment_data = segment
+        self.check_delete_segment(segment_data)
