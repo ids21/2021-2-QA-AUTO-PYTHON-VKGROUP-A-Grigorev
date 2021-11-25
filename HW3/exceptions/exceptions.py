28f93d29b9044936b8bc9c53249bfd82a3f62737
ids21 <49835083+ids21@users.noreply.github.com>
2021-11-25 12:20:01 +0300

    Qa python homework 3 (#6)
    * Edit requirements.txt file
    
    * Prepared test env
    
    * Added test for Campaign api
    
    * Added tests for Segments api:
    Delete and Create
    
    * Added test.jpeg
    
    * CampaignBase and SegmentBase moved to new file.
    Creating method renamed
    
    * Update requirements.txt
    
    Requirements txt updated

---
+++ HW3/exceptions/exceptions.py
@@ -0,0 +1,16 @@
+class BaseException(Exception):
+    def __init__(self, message):    
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+class InvalidLoginException(BaseException):
+    pass
+
+
+class ResponseErrorException(BaseException):
+    pass
+
+class ResponseStatusCodeException(BaseException):
+    pass
