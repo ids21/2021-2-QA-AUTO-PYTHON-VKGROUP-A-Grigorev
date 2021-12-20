c9f1a3738bf78dfe59045f1232ec557a4c03bac5
ids21 <grinch.aleks@gmail.com>
2021-12-20 12:43:33 +0300

    Some mock fix

--- final_project/mock/vk_mock.py
+++ final_project/mock/vk_mock.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask
+import json
 
 
 users = {'Sasha1':1}
@@ -6,21 +7,13 @@ users = {'Sasha1':1}
 mock = Flask(__name__)
 
 
-class VKMock:
+@mock.route('/vk_id/<username>', methods=['GET'])
+def get_vk_id(username):
+    if username in users.keys():
+        return json.dumps({'vk_id': users[username]}), 200
+    else:
+        return json.dumps({}), 404
 
-    @staticmethod
-    @mock.route('/vk_id/<username>', methods=['GET'])
-    def get_vk_id(username):
-        if username in users.keys():
-            return jsonify({'vk_id': users[username]}), 200
-        else:
-            return jsonify({'username is not exist'}), 404
-
-    @mock.route('/add_user/<username>', methods=['POST'])
-    def add_user(username):
-        vk_id = len(users) + 1
-        users[username] = vk_id
-        return f'User {username} added. VK ID: {vk_id}.', 201
 
 if __name__ == '__main__':
     mock.run(host='0.0.0.0', port=9000)
